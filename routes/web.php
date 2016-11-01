a5b92ed0513499cbe6dc717e11f4e4fc2aa553b9
Goran Mijailovic <mi.goran@hotmail.com>
2016-11-01 21:24:20 +0100

    w2

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,29 @@ Route::get('/insert', function(){
     $user->address()->save($address);
 });
 
+
+Route::get('/update', function(){
+
+//    $address = Address::where('user_id', 1)->first();
+//    $address = Address::where('user_id', '=', 1)->first();
+    $address = Address::whereUserId(1)->first();
+
+    $address->name = "Update new address";
+    $address->save();
+
+});
+
+
+
+Route::get('/read', function(){
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+});
+
+
+
+Route::get('/delete', function(){
+    $user = User::findOrFail(1);
+    $user->address()->delete();
+    return "done";
+});
